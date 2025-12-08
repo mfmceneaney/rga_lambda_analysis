970e5498043ac8cd177bc90c16cabc406c9051ef
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-12-08 18:05:59 -0500

    fix: Add momentum correction to data rga jobs.

--- jobs/c12analysis/dt_rga/job.sh
+++ jobs/c12analysis/dt_rga/job.sh
@@ -7,6 +7,6 @@ mkdir -p $OUTDIR
 cd $OUTDIR
 
 # Get proton pion skim for lambda
-$RGA_LAMBDA_ANALYSIS_C12ANALYSIS_COMMAND $INFILE -ch 2212,-211 -be $RGA_LAMBDA_ANALYSIS_BEAM_ENERGY_RGA -tpid $RGA_LAMBDA_ANALYSIS_TPID_RGA -rn -en -ang -vtx -ik -lk -f -out $OUTDIR/skim_ppim_${name}.root
+$RGA_LAMBDA_ANALYSIS_C12ANALYSIS_COMMAND $INFILE -ch 2212,-211 -be $RGA_LAMBDA_ANALYSIS_BEAM_ENERGY_RGA -tpid $RGA_LAMBDA_ANALYSIS_TPID_RGA -rn -en -ang -vtx -ik -lk -f -out $OUTDIR/skim_ppim_${name}.root -momc 'Fall2018' 1 1
 
 echo DONE
