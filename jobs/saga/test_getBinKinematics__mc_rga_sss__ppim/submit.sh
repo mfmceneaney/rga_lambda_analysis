761fbcd1fa68d24bc8941b7d2ea9e9108bb9db34
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-12-09 14:52:47 -0500

    fix: Add MC RGA string spinner directories.

---
+++ jobs/saga/test_getBinKinematics__mc_rga_sss__ppim/submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=saga_getBinKinematics_pi
+#SBATCH --output=/farm_out/%u/%x-%j-%N.out
+#SBATCH --error=/farm_out/%u/%x-%j-%N.err
+#SBATCH --partition=production
+#SBATCH --account=clas12
+#SBATCH -c 4
+#SBATCH --mem-per-cpu=2G
+##SBATCH --gres=disk:1000
+#SBATCH --time=24:00:00
+
+export OUTDIR="$RGA_LAMBDA_ANALYSIS_HOME/jobs/saga/test_getBinKinematics__pi"
+export YAML="args.yaml"
+
+echo $OUTDIR
+echo $YAML
+
+cd $OUTDIR
+ls -lrth
+pwd
+RGA_LAMBDA_ANALYSIS_SAGA_COMMAND "getBinKinematics $YAML"
+echo DONE
