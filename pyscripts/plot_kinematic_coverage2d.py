81f17ae4b307193e0922b98787e940b5a23dbbf2
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-12-09 13:52:47 -0500

    fix: Fix indentation.

--- pyscripts/plot_kinematic_coverage2d.py
+++ pyscripts/plot_kinematic_coverage2d.py
@@ -38,66 +38,66 @@ rg_labels = {'dt_rga':'Data RGA','mc_rga':'MC RGA','mc_rga_SSS':'MC RGA SS'}
 for rg in rgs:
     for ch in chs:
 
-            # Set bin variable labels and limits inside loop since they depend on channel and energy
-            binvar_labels = {
-                'x':'$x$',
-                'Q2':'$Q^{2}$ (GeV)$^{2}$',
-                'W':'$W$ (GeV)',
-            }
-            binvar_lims = {
-                'x':[0.0,1.0],
-                'Q2':[1.0,11.0],
-                'W':[2.0,4.0],
-            }
+        # Set bin variable labels and limits inside loop since they depend on channel and energy
+        binvar_labels = {
+            'x':'$x$',
+            'Q2':'$Q^{2}$ (GeV)$^{2}$',
+            'W':'$W$ (GeV)',
+        }
+        binvar_lims = {
+            'x':[0.0,1.0],
+            'Q2':[1.0,11.0],
+            'W':[2.0,4.0],
+        }
 
-            # Set bin scheme variable pairs to loop
-            binvars_pairs = [['x','Q2'],['x','W']]
+        # Set bin scheme variable pairs to loop
+        binvars_pairs = [['x','Q2'],['x','W']]
 
-            for binvars in binvars_pairs:
+        for binvars in binvars_pairs:
 
-                # Setup, modify these as needed for your specific binning scheme
-                sep = '_'
-                hist_path = os.path.abspath(
-                    os.path.join(
-                        RGA_LAMBDA_ANALYSIS_HOME,
-                        f'jobs/saga/test_getBinKinematicsTH2Ds__{ch}/',
-                        f'out_{rg}_fullbin_binscheme_kinematics.root'
-                    )
+            # Setup, modify these as needed for your specific binning scheme
+            sep = '_'
+            hist_path = os.path.abspath(
+                os.path.join(
+                    RGA_LAMBDA_ANALYSIS_HOME,
+                    f'jobs/saga/test_getBinKinematicsTH2Ds__{ch}/',
+                    f'out_{rg}_fullbin_binscheme_kinematics.root'
                 )
-                hist_name       = 'h2_bin0_'+sep.join(binvars)
-                outpath         = f'kinematic_coverage2d_{binvars[0]}_{binvars[1]}_{rg}_{ch}.pdf'
-                var_keys        = binvars #NOTE: This should only be set in the case of a 2D grid scheme.
-                start_idx       = 0
-                id_key          = 'bin_id'
+            )
+            hist_name       = 'h2_bin0_'+sep.join(binvars)
+            outpath         = f'kinematic_coverage2d_{binvars[0]}_{binvars[1]}_{rg}_{ch}.pdf'
+            var_keys        = binvars #NOTE: This should only be set in the case of a 2D grid scheme.
+            start_idx       = 0
+            id_key          = 'bin_id'
 
-                # Load TH2 histogram with uproot
-                h2 = load_th1(hist_path,name=hist_name)
+            # Load TH2 histogram with uproot
+            h2 = load_th1(hist_path,name=hist_name)
 
-                # Set plt settings
-                set_default_plt_settings()
+            # Set plt settings
+            set_default_plt_settings()
 
-                # Open the figure
-                f, ax = plt.subplots(figsize=(16,10))
+            # Open the figure
+            f, ax = plt.subplots(figsize=(16,10))
 
-                # Plot the 2D distribution
-                plot_th2(h2, ax, norm=colors.LogNorm())
-                ax.set_xlabel(binvar_labels[binvars[0]],usetex=True)
-                ax.set_ylabel(binvar_labels[binvars[1]],usetex=True)
-                ax.set_title(f'{rg_labels[rg]} ${ch_labels[ch]}$',usetex=True)
+            # Plot the 2D distribution
+            plot_th2(h2, ax, norm=colors.LogNorm())
+            ax.set_xlabel(binvar_labels[binvars[0]],usetex=True)
+            ax.set_ylabel(binvar_labels[binvars[1]],usetex=True)
+            ax.set_title(f'{rg_labels[rg]} ${ch_labels[ch]}$',usetex=True)
 
-                # Plot watermark
-                if args.watermark:
-                    watermark = "CLAS12 Preliminary"
-                    plot_watermark(
-                        watermark,
-                        size=75,
-                        rotation=25.0,
-                        color="gray",
-                        alpha=0.5,
-                    )
+            # Plot watermark
+            if args.watermark:
+                watermark = "CLAS12 Preliminary"
+                plot_watermark(
+                    watermark,
+                    size=75,
+                    rotation=25.0,
+                    color="gray",
+                    alpha=0.5,
+                )
 
-                # Save the figure
-                f.savefig(outpath)
+            # Save the figure
+            f.savefig(outpath)
 
-                # Close figures
-                plt.close()
+            # Close figures
+            plt.close()
