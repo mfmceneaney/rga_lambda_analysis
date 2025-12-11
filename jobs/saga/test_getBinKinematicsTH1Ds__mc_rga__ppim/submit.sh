6bd4958a646f4ced74175498b2ffde8a29271996
Matthew F. McEneaney <mfmceneaney@gmail.com>
2025-12-11 08:42:54 -0500

    fix: Fix output directory paths.

--- jobs/saga/test_getBinKinematicsTH1Ds__mc_rga__ppim/submit.sh
+++ jobs/saga/test_getBinKinematicsTH1Ds__mc_rga__ppim/submit.sh
@@ -10,7 +10,7 @@
 ##SBATCH --gres=disk:1000
 #SBATCH --time=24:00:00
 
-export OUTDIR="$RGA_LAMBDA_ANALYSIS_HOME/jobs/saga/test_getBinKinematics__pi"
+export OUTDIR="$RGA_LAMBDA_ANALYSIS_HOME/jobs/saga/test_getBinKinematicsTH1Ds__mc_rga__ppim"
 export YAML="args.yaml"
 
 echo $OUTDIR
