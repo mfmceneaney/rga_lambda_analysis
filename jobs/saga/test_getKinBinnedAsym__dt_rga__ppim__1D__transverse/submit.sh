81ace6c599ad8ab525a7684a14c1b34c846942aa
Matthew F. McEneaney <mfmceneaney@gmail.com>
2026-01-20 20:18:31 -0500

    feat: Add directories for full transverse lambda polarization fits.

---
+++ jobs/saga/test_getKinBinnedAsym__dt_rga__ppim__1D__transverse/submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=saga_getKinBinnedAsym
+#SBATCH --output=/farm_out/%u/%x-%j-%N.out
+#SBATCH --error=/farm_out/%u/%x-%j-%N.err
+#SBATCH --partition=production
+#SBATCH --account=clas12
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=2G
+##SBATCH --gres=disk:5000
+#SBATCH --time=24:00:00
+
+export OUTDIR="$RGA_LAMBDA_ANALYSIS_HOME/jobs/saga/test_getKinBinnedAsym__dt_rga__ppim__1D__transverse"
+export YAML="args.yaml"
+
+echo $OUTDIR
+echo $YAML
+
+cd $OUTDIR
+ls -lrth
+pwd
+RGA_LAMBDA_ANALYSIS_SAGA_COMMAND "getKinBinnedAsym $YAML"
+echo DONE
